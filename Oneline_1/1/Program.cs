11c24bc1d84b1a3281bbdc0c5acb485e0dfcf7c4
naumov <naumov.a.n@icloud.com>
2022-06-18 16:20:40 +0700

    совмещаем структуру с гитом

---
+++ Oneline_1/1/Program.cs
@@ -0,0 +1,18 @@
+﻿int n1, n2, quad;
+
+Console.Write("Ведите число 1: ");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Ведите число 1: ");
+n2 = Convert.ToInt32(Console.ReadLine());
+
+quad = n2 * n2;
+
+if (quad == n1)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
