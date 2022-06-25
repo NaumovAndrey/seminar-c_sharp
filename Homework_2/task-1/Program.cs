3f44c1a75b1f55fced659955b607af70083084e0
naumov <naumov.a.n@icloud.com>
2022-06-25 19:42:31 +0700

    добавленно описание к дз2-1

--- Homework_2/task-1/Program.cs
+++ Homework_2/task-1/Program.cs
@@ -5,24 +5,24 @@
 918 -> 1
 */
 
-int second_index(int nums)
+int second_index(int nums)                               // метод. вычислям среднюю цифру
 {
-    int result = (nums / 10) % 10;
-    return result;
+    int result = (nums / 10) % 10;                       // записываем результат входных данных в переменную 'result'
+    return result;                                       // выходные данные 
 }
 
 
 while (true) 
 {
     Console.WriteLine("Введите трёхзначное число: ");    // запрос на ввод трёхзначного числа
-    string text = Console.ReadLine();
-    bool text_num = int.TryParse(text, out int num);
+    string text = Console.ReadLine();                    // ввод от пользователя
+    bool text_num = int.TryParse(text, out int num);     // проверяем ввод на число, если истина, значит конвертируем в int и записываем в переменную num
     if (text_num)
     {
-        if (num < 1000 && num > 99)
+        if (num < 1000 && num > 99)                      // проверяем число на трёхзначное
         {
-            int res = second_index(num);
-            Console.WriteLine(res);
+            int res = second_index(num);                 // обращаемся к методу 'second_index'
+            Console.WriteLine(res);                      // и конечно же выводим в консоль
             break;
         }
             
