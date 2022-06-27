149b413247e311488a12192f9ace3c93df8b590a
naumov <naumov.a.n@icloud.com>
2022-06-28 00:03:01 +0700

    update 2 and 3

---
+++ Homework_2/task-2_1/Program.cs
@@ -0,0 +1,55 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+*/
+
+class Program
+{
+    static int num(string user_num)
+    {
+        Console.Write(user_num);
+        string str = Console.ReadLine();
+        int u_num = Convert.ToInt32(str);
+ 
+        return u_num;
+    }
+    static int[] number_length(int num) 
+    {
+        num = Math.Abs(num);
+        List<int> numbers = new List<int>();
+        while (num > 0)
+        {
+            numbers.Add(num % 10);
+            num /= 10;
+        }
+        numbers.Reverse();
+        return numbers.ToArray();
+ 
+    }
+    static void printLackOfData()
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+    static void printIncorrectData()
+    {
+        Console.WriteLine("Некорректный ввод");
+    }
+
+    static void Main(string[] args) {
+        try {
+            int user_input = num("Введите положительное целое число: ");
+            int[] number_processing = number_length(user_input);
+            Console.WriteLine(number_processing[2]); 
+        }
+        catch (IndexOutOfRangeException) {              
+            printLackOfData();
+        }
+        catch (Exception) {
+            printIncorrectData();
+        }
+    }
+}
--- Homework_2/task-3/Program.cs
+++ Homework_2/task-3/Program.cs
@@ -3,7 +3,7 @@
 6 -> да
 7 -> да
 1 -> нет
- */
+
 
 Console.Write("Введите номер дня недели: ");
 int day = Convert.ToInt32(Console.ReadLine());
@@ -11,3 +11,26 @@ int day = Convert.ToInt32(Console.ReadLine());
 if (day == 1 || day == 2 || day == 3 || day == 4 || day == 5) Console.Write("Нет");
 else if (day == 6 || day == 7) Console.WriteLine("Да");
 else Console.WriteLine("Ошибка");
+*/
+
+
+class Program {
+    static int num(string user_num)
+    {
+        Console.Write(user_num);
+        string str = Console.ReadLine();
+        int u_num = Convert.ToInt32(str);
+ 
+        return u_num;
+    }
+    static string day(int num) {
+        int[] i = {1, 2, 3, 4, 5, 6, 7};
+        if ()
+    }
+
+    static void Main(string[] args) {
+        int uNum = num("Введите номер дня недели: ");
+        string str = day(uNum);
+        Console.WriteLine("Результат: " + str);
+    }
+}
