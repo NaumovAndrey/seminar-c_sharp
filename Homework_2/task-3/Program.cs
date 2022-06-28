0d20da04fdfc5a3f0ba850dff88f663780c4c56a
naumov <naumov.a.n@icloud.com>
2022-06-28 23:44:42 +0700

    update dz2-3

--- Homework_2/task-3/Program.cs
+++ Homework_2/task-3/Program.cs
@@ -3,34 +3,35 @@
 6 -> да
 7 -> да
 1 -> нет
-
-
-Console.Write("Введите номер дня недели: ");
-int day = Convert.ToInt32(Console.ReadLine());
-
-if (day == 1 || day == 2 || day == 3 || day == 4 || day == 5) Console.Write("Нет");
-else if (day == 6 || day == 7) Console.WriteLine("Да");
-else Console.WriteLine("Ошибка");
 */
 
-
-class Program {
-    static int num(string user_num)
+class Program 
+{
+    static int u_num(string t_num)
     {
-        Console.Write(user_num);
+        Console.WriteLine(t_num);
         string str = Console.ReadLine();
-        int u_num = Convert.ToInt32(str);
- 
-        return u_num;
+        int num = Convert.ToInt32(str);
+        return num;
     }
-    static string day(int num) {
-        int[] i = {1, 2, 3, 4, 5, 6, 7};
-        if ()
+    static void calculation(int usnum)
+    {
+        if (usnum < 6 && usnum > 0)
+        {
+            Console.WriteLine("NO");
+        }
+        else if (usnum < 8 && usnum > 5)
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("Enter a number from 1 to 7");
+        }
     }
-
-    static void Main(string[] args) {
-        int uNum = num("Введите номер дня недели: ");
-        string str = day(uNum);
-        Console.WriteLine("Результат: " + str);
+    static void Main(string[] args)
+    {
+        int usnum = u_num("Введите номер дня недели: ");
+        calculation(usnum);
     }
 }
