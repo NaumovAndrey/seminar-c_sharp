23c53c6cb4d8c0e51ba1b61cce3d0cafe952421a
naumov <naumov.a.n@icloud.com>
2022-07-10 00:44:36 +0700

    dz5-1

---
+++ Homework_5/task5-1/Program.cs
@@ -0,0 +1,41 @@
+﻿/*  Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2   */
+
+int userNum(string num)
+{
+    Console.Write(num);
+    string text = Console.ReadLine();
+    int nums = Convert.ToInt32(text);
+    return nums;
+}
+int[] CreateRandomArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++) { array[i] = new Random().Next(100, 1000); }
+    return array;
+}
+int positive_numbers(int[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] % 2 == 0) { counter += 1;}
+    return counter;
+}
+string ShowArray(int[] array)
+{
+    string mas = string.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1) { mas = mas + array[i]; }
+        else { mas = mas + array[i] + ", "; }
+    }
+    return mas;
+}
+
+int num = userNum("Введите длину массива: ");
+int[] Array = CreateRandomArray(num);
+int counter = positive_numbers(Array);
+string mas = ShowArray(Array);
+Console.WriteLine("В массиве: [" + mas + "] " + counter + " положительных числа!");
+
---
+++ Oneline_6/draft/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] CreateRandomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Ввежите длину массива: ");
+int size = Console.ReadLine();
+Console.WriteLine("Ввежите min значение массива: ");
+int min = Console.ReadLine();
+Console.WriteLine("Ввежите max значение массива: ");
+int max = Console.ReadLine();
