a950cc9f7cb4b126e0a86169e81d69d4d09d4728
naumov <naumov.a.n@icloud.com>
2022-07-15 01:20:27 +0700

    dz6-1

---
+++ Homework_6/task6-2/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Напишите программу, которая найдёт точку пересечения двух прямых,
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+*/
+
+double UserNum(string text)
+{
+    Console.Write(text);
+    return Convert.ToDouble(Console.ReadLine());
+}
+Double IntersectionsLines(double b1, double k1, double b2, double k2)
+{
+    return (b2 - b1) / (k1 - k2);
+}
+
+double b1 = UserNum("Введите первую точку прямой AB: ");
+double k1 = UserNum("Введите вторую точку прямой AB: ");
+double b2 = UserNum("Введите первую точку прямой CD: ");
+double k2 = UserNum("Введите вторую точку прямой CD: ");
+double x = IntersectionsLines(b1, k1, b2, k2);
+Console.WriteLine(x);
+
---
+++ Oneline_7/task1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+
+int[,] CreateRandom2Array(int rows, int colums, int minValue, int maxValue)
+{
+    int[,] newArray = new int[rows, colums];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < colums; j++) { newArray[i, j] = new Random().Next(minValue, maxValue + 1); }
+        return newArray;
+}
+void Show2Array(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.WriteLine(i, j + " ");
+        }
+        Console.WriteLine();
+    }
+}
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] myArray = CreateRandom2Array(m,n, 1, 9);
+Show2Array(myArray);
