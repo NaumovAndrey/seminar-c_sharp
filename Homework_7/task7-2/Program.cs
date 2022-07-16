e40b5782661cfe1d3454d98ac5c3435e45929cb9
naumov <naumov.a.n@icloud.com>
2022-07-16 20:06:21 +0700

    dz7-2 Arrays. The randomness of the rows and columns of the array. Searching for a value in an array.Arrays. The randomness of the rows and columns of the array. Searching for a value in an array

---
+++ Homework_7/task7-2/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+1 7 -> такого числа в массиве нет
+*/
+
+// в методе используются магическме числа для упращения читаемости вывода.
+
+void Array(int line, int column) {
+    int meaningLine = new Random().Next(3, 10);
+    int meaningColumn = new Random().Next(3, 10);
+    int[,] array = new int[meaningLine, meaningColumn];
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) { 
+            array[i, j] = new Random().Next(0, 10);
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    if (line > meaningLine || column > meaningColumn || line < 1 || column < 1) {
+        Console.WriteLine("Такого элемента в массиве нет."); 
+    }
+    else { Console.WriteLine($"Значение в массиве = {array[line - 1, column - 1]}");}
+
+}
+
+
+Console.Write("Введите позицию строки в массиве: ");
+int line = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите позицию столбца в массиве: ");
+int column = Convert.ToInt32(Console.ReadLine());
+Array(line, column);
---
+++ Oneline_8/task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+int[,] RevesArray(int[,] array)
+{
+    if (array.GetLength(0) != array.GetLength(1))
+    {
+        Console.WriteLine("No");
+        return array;
+    }
+    for (int i = 0; i < array.GetLength(0) - 1; i++)
+        for (int j = i + 1; j < array.GetLength(1); j++)
+        {
+            int temp = array[i, j];
+            array[i, j] = array[j, i];
+            array[j, i] = temp;
+        }
+    return array;
+}
