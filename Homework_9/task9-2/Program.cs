20bc6b8a04f2a70753eb50fed7313dc8dda1ddbb
naumov <naumov.a.n@icloud.com>
2022-07-30 00:33:37 +0700

    dz9-2

--- Homework_9/task9-2/Program.cs
+++ Homework_9/task9-2/Program.cs
@@ -3,19 +3,29 @@ M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
 */
 
+
 int input(string num) {
     Console.Write(num);
     return int.Parse(Console.ReadLine());
 }
-int Sum(int m, int n, int i = 0) { 
-    if (m > n) {
-        i += (m++);
-        return Sum(m, n);
+int[] CreateList(int m, int n) 
+    {
+        List<int> numbers = new List<int>();
+        while (m <= n)
+        {
+            numbers.Add(m);
+            m += 1;
+        }
+        return numbers.ToArray();
     }
-    else return 1;
+int Sum(int[] myArray, int i = 0) { 
+    if (i >= myArray.Length) return 0;
+    int result = Sum(myArray, i+1);
+    return myArray[i] + result;
 }
 
 int m = input("Введите начальное число: ");
 int n = input("Введите конечное число: ");
-int sum = Sum(m, n);
-Console.WriteLine($"Сумма натуральных чисел от {m} до {n} = {sum}");
+int[] array = CreateList(m, n);
+int result = Sum(array);
+Console.WriteLine($"Сумма натуральных чисел от {m} до {n} = {result}");
