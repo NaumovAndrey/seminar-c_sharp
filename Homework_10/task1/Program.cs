25e5b5dd39d0e18ddf96ef03e72bf4f837a730ba
naumov <naumov.a.n@icloud.com>
2022-07-30 19:44:55 +0700

    dz10-1

--- Homework_10/task1/Program.cs
+++ Homework_10/task1/Program.cs
@@ -8,17 +8,18 @@ string[] TextArray(string text) {
     string[] userTextArray = userText.Split(new char[] { ' ' }); 
     return userTextArray;
 }
-int numberOfWords(string[] textArray, char cymbol, int count = 0) { 
-    const cymbol: set of char{ 'a', 'e', 'i', 'o', 'u', 'y'};
-    //cymbol = {a, e, i, o, u, y};
+int numberOfWords(string[] textArray, int count = 0) { 
+    char[] simbol = new char[] {'a', 'e', 'i', 'o', 'u', 'y'};
     for (int i = 0; i < textArray.Length; i++) {
-        if (textArray[i].ToLower()[0] == char.ToLower(cymbol)) {
+        for (int j = 0; j < simbol.Length; j++) {
+            if (textArray[i].ToLower()[0] == char.ToLower(simbol[j])) {
             count++;
+            }
         }
     }
     return count;
 }
+
 string[] text = TextArray("Введите текст для подсчета слов начинающихся на гласную букву: ");
 int count = numberOfWords(text);
 Console.WriteLine($"В данном тексте содержится {count} слов начинающихся на гласную букву");
-
